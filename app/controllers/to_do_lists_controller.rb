b4966d368f14442a3f022bfe3f45f000077ac0b1
Britni <BAlexander@Britnis-MacBook-Pro.local>
2017-02-22 20:06:04 -0500

    Create and show a list

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -5,4 +5,24 @@ class ToDoListsController < ApplicationController
   def new
     @to_do_list = ToDoList.new
   end
+
+  def create
+    @to_do_list = ToDoList.new(to_do_list_params)
+    if @to_do_list.save
+      flash[:success] = 'To do list created successfully'
+      render :show
+    else
+      flash[:error] = 'There is a problem in creating the to-do list'
+      render :new
+    end
+  end
+
+  def show
+    @to_do_list = ToDoList.find(params[:id])
+  end
+
+  private
+  def to_do_list_params
+    params.require(:to_do_list).permit(:name)
+  end
 end
